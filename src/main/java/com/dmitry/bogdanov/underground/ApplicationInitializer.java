4dccfbc2793ac0f8011eaa6b4f76cf71608e5be9
dimi3y <dmitry.bogdanov213@gmail.com>
2017-05-18 18:28:46 +0300

    Added application initializer class Added web configuration class

---
+++ src/main/java/com/dmitry/bogdanov/underground/ApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.dmitry.bogdanov.underground;
+
+import com.dmitry.bogdanov.underground.config.WebConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+
+        servletContext.addListener(new ContextLoaderListener(ctx));
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
